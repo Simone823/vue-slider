2030d4717c952d6af0bd53a675456b29c50ae99f
Simone823 <simone.daglio823@outlook.it>
2022-02-24 16:11:17 +0100

    vuejs, data arrayItems

---
+++ app.js
@@ -0,0 +1,43 @@
+// Vue JS
+const container = new Vue({
+    el: ".container",
+    data: {
+        // Array oggetti
+        arrayItems: [
+        {
+            immagine: "img/01.jpg",
+            titolo: "Svezia",
+            testo: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+        },
+    
+        {
+            immagine: "img/02.jpg",
+            titolo: "Svizzera",
+            testo: "Lorem ipsum",
+        },
+    
+        {
+            immagine: "img/03.jpg",
+            titolo: "Gran Bretagna",
+            testo: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+        },
+    
+        {
+            immagine: "img/04.jpg",
+            titolo: "Germania",
+            testo: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
+        },
+    
+        {
+            immagine: "img/05.jpg",
+            titolo: "Paradise",
+            testo: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
+        }],
+    }
+})
+console.log(container);
+
+
+
+
+
